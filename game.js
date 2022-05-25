b9680c8568ab914a760f4e1a4ad6d29733ef9851
Marcel Schliwka <marcelschliwka@protonmail.com>
2022-05-25 15:12:34 +0200

    Main Play Function

--- game.js
+++ game.js
@@ -27,12 +27,41 @@ function playRound(usersChoice, computersChoice) {
         usersChoice == "scissor" && computersChoice == "scissor" ||
         usersChoice == "paper" && computersChoice == "paper") {
             console.log(`Tie! Computer Choose ${computersChoice.toUpperCase()} and you choose ${usersChoice.toUpperCase()}`);
+            return "Both";
     } else if (usersChoice == "rock" && computersChoice == "scissor" ||
                 usersChoice == "paper" && computersChoice == "rock" ||
                 usersChoice == "scissor" && computersChoice == "paper") {
                     console.log(`You Won! Computer Choose ${computersChoice.toUpperCase()} and you choose ${usersChoice.toUpperCase()}`);
+                    return "User";
                 } else {
                     console.log(`You loose! Computer Choose ${computersChoice.toUpperCase()} and you choose ${usersChoice.toUpperCase()}`);
+                    return "Computer";
                 }
 
+}
+
+function play() {
+    let userPoints = 0;
+    let computerPoints = 0;
+    for(let i = 0; i < 5; i++){
+        usersChoice = prompt("Coose Rock, Paper or Scissor!").toLowerCase();
+        let winner = playRound(usersChoice);
+        if (winner == "User") {
+            userPoints++;
+        } else if (winner == "Computer") {
+            computerPoints++;
+        } else {
+            userPoints++;
+            computerPoints++;
+        }
+    }
+
+    if (userPoints > computerPoints) {
+        console.log(`You Won! with User: ${userPoints} and Computer: ${computerPoints}`)
+    } else if (userPoints == computerPoints) {
+        console.log(`Tie! You have the same amount of Points User: ${userPoints} and Computer: ${computerPoints}`)
+    }
+    else {
+        console.log(`You lost! with User: ${userPoints} and Computer: ${computerPoints}`)
+    }
 }
